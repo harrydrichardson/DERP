e523fb306619d49d7dfe8ba560cf8edbed0bd502
Harry Richardson <richardson.chesney@gmail.com>
2014-07-19 06:37:18 -0400

    added authorization check

--- accept.php
+++ accept.php
@@ -2,6 +2,21 @@
 
 require('config.php');
 
+// slack post icon
+define("slackemoji",":derp:");
+
+$channels = array("support","support-ss");
+$admins = array("harry");
+
+	if($_POST['token'] != slackout 
+		|| !in_array($_POST['channel_name'],$ALLOWED_CHANNELS))
+	{
+		die('Not Authorized');
+	}
+	
+$command = explode("[derp]",strtolower($_POST['text']));
+
+
 	//debugs if DEBUG_MODE is true
 	function debug($debugMsg)
 	{
