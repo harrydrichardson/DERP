abebfa0cc914430abdb853f06df7b02665d58939
Harry Richardson <richardson.chesney@gmail.com>
2014-07-19 22:53:22 -0400

    Working, echos most recent command with formatting options

--- accept.php
+++ accept.php
@@ -7,48 +7,61 @@ define("slackemoji",":derp:");
 
 $channels = array("testing-webhooks","support","support-ss");
 $admins = array("harry","tyler");
-$hello = "herp";
-//echo "Token: " . $_POST['token'] . "</br>";
-//echo "Channel: " . $_POST['channel_name'] . "</br>";
-	if($_POST['token'] != slackout 
-		|| !in_array($_POST['channel_name'],$channels))
-	{
-		die('Not Authorized');
-	}
 
-$_POST['channel_name'] = "#".$_POST['channel_name']; 
-	
-	if( $_POST['user_name'] == 'harry' || $_POST['user_name'] == 'tyler'){
-		messageToSlack("herp",$_POST['channel_name']);
+		// Ensure that incoming post has proper auth.
+		// Ensure that it is an authorized channel
+        if($_POST['token'] != slackout
+                || !in_array($_POST['channel_name'],$channels))
+        {
+                die('Not Authorized');
+        }
+
+// split posted timestamp = 123123423.00020340
+$timestamp = explode(".",$_POST['timestamp']);
+//convert to GMT
+$_POST['timestamp'] = gmdate('r',$timestamp[0]);
+
+// format channel name for the expectation of the incoming hook
+$_POST['channel_name'] = "#".$_POST['channel_name'];
+
+
+$keys = array_keys($_POST);
+	if($_POST['user_name'] == 'andrewherrington'){
+		messageToSlack("You are why we can't have nice things",$_POST['channel_name']);
+}
+        if( $_POST['user_name'] == 'harry' || $_POST['user_name'] ==  'tyler'){
+                	
+                messageToSlack($_POST['user_name'].": ".$_POST['timestamp']." -- '".$_POST['text']."'",$_POST['channel_name']);
 		die();
-	}
+                }
+        
 $payload = explode("[derp]",strtolower($_POST['text']));
-$token = strtok($payload[1]," ");
 
 
 
 
-	//debugs if DEBUG_MODE is true
-	function debug($debugMsg)
-	{
-		if(DEBUG_MODE)
-		{
-			$file = "/var/www/html/public/debug.log";
-			file_put_contents($file,$debugMsg."\r\n",FILE_APPEND);
-		}
-	}
+        //debugs if DEBUG_MODE is true
+        function debug($debugMsg)
+        {
+                if(DEBUG_MODE)
+                {
+                        $file = "/var/www/html/public/debug.log";
+                        file_put_contents($file,$debugMsg."\r\n",FILE_APPEND);
+                }
+        }
 
-	function messageToSlack($message,$channel)
-	{
-		$broadcastURL = slackurl.slackin;
-        	$data= 'payload={"username": "DERP", "channel": "'.$channel.'", "text": "';
-		$data.=$message.'", "icon_emoji": "'.slackemoji.'"}';
-		//	debug($data);
-		//	debug($broadcastURL);
-        	$ch = curl_init($broadcastURL);
-	        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-      		return curl_exec($ch);
-	}	
+        function messageToSlack($message,$channel)
+        {
+                $broadcastURL = slackurl.slackin;
+                $data= 'payload={"username": "DERP", "channel": "'.$channel.'", "text": "';
+                $data.=$message.'", "icon_emoji": "'.slackemoji.'"}';
+                //      debug($data);
+                //      debug($broadcastURL);
+                $ch = curl_init($broadcastURL);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+                return curl_exec($ch);
+        }
 
 
 ?>
+
