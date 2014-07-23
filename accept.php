fcd66df1545e2b825bdd35c4300331b19b0dc461
Harry Richardson <richardson.chesney@gmail.com>
2014-07-23 06:29:59 -0400

    setup the remove trigger code.

--- accept.php
+++ accept.php
@@ -2,14 +2,28 @@
 
 require('config.php');
 
+//remove trigger word from string
+$message = $_POST['text'];
+//split at the sentences spaces.
+$message = explode(" ",$message);
+//remove the slack triggerword
+unset($message[0]);
+//setup the message to send back without the trigger word
+$readBackMessage = "";
+//loop through the array assembling the sentence
+foreach($message as $word){
+	$readBackMessage .= "$word ";
+}
+	
+
 // slack post icon
 define("slackemoji",":derp:");
 
 $channels = array("testing-webhooks","support","support-ss");
 $admins = array("harry","tyler");
 
-		// Ensure that incoming post has proper auth.
-		// Ensure that it is an authorized channel
+                // Ensure that incoming post has proper auth.
+                // Ensure that it is an authorized channel
         if($_POST['token'] != slackout
                 || !in_array($_POST['channel_name'],$channels))
         {
@@ -19,25 +33,21 @@ $admins = array("harry","tyler");
 // split posted timestamp = 123123423.00020340
 $timestamp = explode(".",$_POST['timestamp']);
 //convert to GMT
-$_POST['timestamp'] = gmdate('r',$timestamp[0]);
+$_POST['timestamp'] = gmdate("M d Y H:i:s",$timestamp[0]);
 
 // format channel name for the expectation of the incoming hook
 $_POST['channel_name'] = "#".$_POST['channel_name'];
 
 
-$keys = array_keys($_POST);
-	if($_POST['user_name'] == 'andrewherrington'){
-		messageToSlack("You are why we can't have nice things",$_POST['channel_name']);
+		// Jab fun at Andrew
+        if($_POST['user_name'] == 'andrewherrington'){
+                messageToSlack("You are why we can't have nice things",$_POST['channel_name']);
 }
         if( $_POST['user_name'] == 'harry' || $_POST['user_name'] ==  'tyler'){
-                	
-                messageToSlack($_POST['user_name'].": ".$_POST['timestamp']." -- '".$_POST['text']."'",$_POST['channel_name']);
+
+                messageToSlack($_POST['user_name'].": ".$_POST['timestamp']." -- Message Received contains ".count($message)." elements. Message reads '".$readBackMessage."'",$_POST['channel_name']);                
 		die();
                 }
-        
-$payload = explode("[derp]",strtolower($_POST['text']));
-
-
 
 
         //debugs if DEBUG_MODE is true
@@ -62,6 +72,11 @@ $payload = explode("[derp]",strtolower($_POST['text']));
                 return curl_exec($ch);
         }
 
+	function process($command,$data){
+		$returns = "";
+		
 
+		
+	}
 ?>
 
