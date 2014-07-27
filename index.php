98424cc42aedfc901c822c3c82671f32cd8ec2ad
Harry Richardson <richardson.chesney@gmail.com>
2014-07-27 02:17:58 -0400

    small form for shiftnote.php

---
+++ index.php
@@ -0,0 +1,5 @@
+		<form name="test" action="shiftnote.php" method="POST">
+			Date: <input type="text" name="date" ><br >
+		<input type="hidden" name="form" value="1" >
+		<br >
+		<input type="submit" >
